7d1ca0ee8dbb0c70adb0118a35c9d81e56277384
Andrey <saowwll@gmail.com>
2022-09-03 01:03:22 +0200

    st

--- 04.2/main.go
+++ 04.2/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+type ListNode struct {
+	val  int
+	next *ListNode
+}
+
+func createList(s []int) {
+	node := &ListNode{s[0], &ListNode{}}
+	for k, v := range s {
+		if k > 0 {
+			node.next = &ListNode{v, &ListNode{}}
+			node = node.next
+		}
+		fmt.Println(node)
+	}
+
+}
+
 func main() {
-	
+	s := []int{1, 2, 3}
+	createList(s)
+
 }
