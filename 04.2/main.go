ce335533161c77d448c572f883dab127a0b5be5d
Andrey <saowwll@gmail.com>
2022-09-03 23:29:09 +0200

    wtf

--- 04.2/main.go
+++ 04.2/main.go
@@ -1,24 +1,40 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	val  int
 	next *ListNode
 }
 
 func createList(s []int) *ListNode {
-	first := ListNode{s[0], &ListNode{}}
-	prev := &first
+	first := ListNode{val: s[0]}
+	current := &first
 	for k, v := range s {
 		if k > 0 {
-			prev.next = &ListNode{}
-			prev.next.val = v
-			prev = prev.next
+			current.next = &ListNode{val: v}
+			current = current.next
 		}
 	}
 	return &first
 }
+
 func main() {
-	s := []int{1, 2, 3}
-	a := createList(s)
-	print(a)
+	s := []int{1, 2, 1, 3, 4, 3, 3, 4}
+	list := createList(s)
+	deleteDuplicates(list)
+	fmt.Println(list)
 }
+
+func deleteDuplicates(mainNode *ListNode) *ListNode {
+	current := mainNode
+	for current.next != nil {
+		current = current.next
+	}
+
+	return mainNode
+}
+
+//func printList(a *ListNode) *ListNode {
+//	return nil
+//}
